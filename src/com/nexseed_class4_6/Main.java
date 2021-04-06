7b302ba801dd51b92b0254d884ab4e70bb97a31c
trisha ann balderas <centrii52@gmail.com>
2021-04-06 19:42:44 +0800

    updated code

--- src/com/nexseed_class4_6/Main.java
+++ src/com/nexseed_class4_6/Main.java
@@ -1,15 +1,40 @@
 package com.nexseed_class4_6;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
 
-        GenBoys boys = new GenBoys();
-        boys.bname = "Juan";
-        GenGirls girls = new GenGirls();
-        girls.gname = "Maria";
-        GenRelationship relation = new GenRelationship();
-        relation.relationship = " is a son of ";
-        System.out.println(boys.bname + relation.relationship + girls.gname);
+        GenBoys boy1 = new GenBoys();
+        boy1.bname = "Rodrigo";
+        GenBoys boy2 = new GenBoys();
+        boy2.bname = "Alan";
+        GenBoys boy3 = new GenBoys();
+        boy3.bname = "Chiz";
+        GenGirls girl1 = new GenGirls();
+        girl1.gname = "Heart";
+        GenGirls girl2 = new GenGirls();
+        girl2.gname = "Maria";
+        GenGirls girl3 = new GenGirls();
+        girl3.gname = "Ellen";
+        GenRelationship relation1 = new GenRelationship();
+        relation1.relationship = " is a son of ";
+        GenRelationship relation2 = new GenRelationship();
+        relation2.relationship = " a sibling of ";
+        GenRelationship relation3 = new GenRelationship();
+        relation3.relationship = " friends with ";
+
+        GenBoys[] boys = {boy1, boy2, boy3};
+        GenGirls[] girls = {girl1, girl2, girl3};
+        GenRelationship[] rltn = {relation1, relation2, relation3};
+
+        int count =0;
+        while (count<3) {
+            System.out.println(boys[random()].bname + rltn[random()].relationship + girls[random()].gname);
+            count++;
+        }
+    }
+    public static int random(){
+        Random ran = new Random();
+        return ran.nextInt(3);
     }
 }
